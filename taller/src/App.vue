1e55d354d2e3ab489e90f0a47ceaedfa7e54ec4b
cris <c.cariqueo03@ufromail.cl>
2024-10-09 15:13:25 -0300

    consumo de api en bruto
    se consigue consumir las api y se muestra en pantalla para comprobar coneccion

--- taller/src/App.vue
+++ taller/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <apiConsumo />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import apiConsumo from './components/api-consumo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    apiConsumo
   }
 }
 </script>
---
+++ taller/src/components/api-consumo.vue
@@ -0,0 +1,47 @@
+<template>
+    <!-- dejarle evento que llame funcion -->
+    <button @click="populationData">Datos Población</button>
+    <button @click="animeRecomendationData">Recomendaciones anime</button>
+    <button @click="animeData">Random anime</button>
+    <button @click="mangaData">Random manga</button>
+
+    <p>
+        {{ shownData }}
+    </p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const shownData = ref([]);
+
+// funcion llamada para llamar endpoint
+const populationData = async () => {
+    const response = await fetch('https://countriesnow.space/api/v0.1/countries/population');
+    const populationData = await response.json();
+    shownData.value = populationData.data;
+    console.log(populationData.data);
+};
+const animeRecomendationData = async () => {
+    const response = await fetch('https://api.jikan.moe/v4/recommendations/anime');
+    const animeRecomendation = await response.json();
+    shownData.value = animeRecomendation.data;
+    console.log(animeRecomendation.data);
+};
+const animeData = async () => {
+    const response = await fetch('https://api.jikan.moe/v4/random/anime');
+    const animeData = await response.json();
+    shownData.value = animeData.data;
+    console.log(animeData.data);
+};
+const mangaData = async () => {
+    const response = await fetch('https://api.jikan.moe/v4/random/manga');
+    const mangaData = await response.json();
+    shownData.value = mangaData.data;
+    console.log(mangaData.data);
+};
+</script>
+
+<style>
+
+</style>
